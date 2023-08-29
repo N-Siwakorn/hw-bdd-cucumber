d4cdd3ee3e1b83ebc0ba4da6668e86b6ff99d2a9
N-Siwakorn <siwakornpan25@gmail.com>
2023-08-29 11:20:08 +0700

    part2+3

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  pending "Fill in this step in movie_steps.rb"
+  # pending "Fill in this step in movie_steps.rb"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -14,6 +15,7 @@ end
 
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
+
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
@@ -25,22 +27,54 @@ end
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+
+When /I (un)?check the following ratings: (.*)/ do |checked, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  pending "Fill in this step in movie_steps.rb"
+  ratings = rating_list.split(' ')
+  ratings.each { |rating|
+  if not checked
+    check("ratings[#{rating}]")
+  else
+    uncheck("ratings[#{rating}]")
+  end
+}
+end
+
+When /I check all the ratings/ do
+  ratings = Movie.all_ratings
+  ratings.each { |rating|
+    check("ratings[#{rating}]")
+}
+# ratings = ["R"]
+# ratings.each { |rating|
+# uncheck("ratings[#{rating}]")}
+end
+
+When /I click on "([^"]*)"/ do |button|
+  click_button(button)
 end
 
 # Part 2, Step 3
 Then /^I should (not )?see the following movies: (.*)$/ do |no, movie_list|
   # Take a look at web_steps.rb Then /^(?:|I )should see "([^"]*)"$/
-  pending "Fill in this step in movie_steps.rb"
+  movies = movie_list.split(',')
+  movies.each{ |movie|
+    if not no
+      expect(page).to have_content(movie)
+    else
+      expect(page).not_to have_content(movie)
+    end
+  }
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  pending "Fill in this step in movie_steps.rb"
+  movies = Movie.all
+  movies.each{ |movie|
+    expect(page).to have_content(movie.title)
+  }
 end
 
 ### Utility Steps Just for this assignment.
